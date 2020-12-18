6dd45b45d1e4f9728ffc435f947f1cc1f43ca630
ekadyah <ekadiahcahyani2007@gmail.com>
2020-12-18 15:26:33 +0700

    latihan

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', function(req, res){
   res.send('Hello My name is YOUR_NAME');
 });
 
+app.post("/", function (req, res) {
+  res.send("Hello My name is YOUR_NAME but it from post");
+});
+
+app.put("/", function (req, res) {
+  res.send("Hello My name is YOUR_NAME but it from put");
+});
+
+app.patch("/", function (req, res) {
+  res.send("Hello My name is YOUR_NAME but it from pacth");
+});
+
+app.delete("/", function (req, res) {
+  res.send("Hello My name is YOUR_NAME but it from delete");
+});
+
 app.listen(port, function(){
   console.log('Hello my apps is running on port '+port);
 });
+
